45fb9f329e3b0fe4a11b8243658587ba08d7d429
Ilya Suzdalnitski <ilya.s@replicon.com>
2019-08-02 02:28:39 +0000

    test coverage

--- test/saferr.test.js
+++ test/saferr.test.js
@@ -3,16 +3,84 @@ import saferr from "../src";
 describe("saferr", () => {
   describe("when regular function", () => {
     describe("when throws", () => {
-      it("should return error as first esement of the tuple", () => {
-        const testFunc = () => {
-          throw "error";
-        };
+      const testFunc = () => {
+        throw "error";
+      };
 
-        const safeTestFunc = saferr(testFunc);
+      const safeTestFunc = saferr(testFunc);
 
-        const result = safeTestFunc();
+      it("should return error as first element of the tuple", () => {
+        const [error] = safeTestFunc();
 
-        expect(result).toEqual(["error", null]);
+        expect(error).toEqual("error");
+      });
+
+      it("should return null as second element of the tuple", () => {
+        const [_, result] = safeTestFunc();
+
+        expect(result).toEqual(null);
+      });
+    });
+
+    describe("when does not throw", () => {
+      const testFunc = () => {
+        return "result";
+      };
+
+      const safeTestFunc = saferr(testFunc);
+
+      it("should return null as first element of the tuple", () => {
+        const [error] = safeTestFunc();
+
+        expect(error).toEqual(null);
+      });
+
+      it("should return result as second element of the tuple", () => {
+        const [_, result] = safeTestFunc();
+
+        expect(result).toEqual("result");
+      });
+    });
+  });
+
+  describe("when async function", () => {
+    describe("when throws", () => {
+      const testFunc = async () => {
+        throw "error";
+      };
+
+      const safeTestFunc = saferr(testFunc);
+
+      it("should return error as first element of the tuple", async () => {
+        const [error] = await safeTestFunc();
+
+        expect(error).toEqual("error");
+      });
+
+      it("should return null as second element of the tuple", async () => {
+        const [_, result] = await safeTestFunc();
+
+        expect(result).toEqual(null);
+      });
+    });
+
+    describe("when does not throw", () => {
+      const testFunc = async () => {
+        return "result";
+      };
+
+      const safeTestFunc = saferr(testFunc);
+
+      it("should return null as first element of the tuple", async () => {
+        const [error] = await safeTestFunc();
+
+        expect(error).toEqual(null);
+      });
+
+      it("should return result as second element of the tuple", async () => {
+        const [_, result] = await safeTestFunc();
+
+        expect(result).toEqual("result");
       });
     });
   });
